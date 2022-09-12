4c8dff747cdeaf2a8a31d1ef1292e9e761c7ff22
unknown <hever11.hdgd@gmail.com>
2022-09-11 20:58:22 -0500

    x

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,16 +1,23 @@
 import {
+  Avatar,
   Button,
   Card,
+  CardActionArea,
   CardActions,
   CardContent,
   CardMedia,
   Container,
   Grid,
+  IconButton,
+  Link,
+  Paper,
   Typography,
 } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Box } from "@mui/system";
+import { Copyright } from "@mui/icons-material";
 
 export const Details = () => {
   const [character, setCharacter] = useState(null);
@@ -45,24 +52,22 @@ export const Details = () => {
       </Typography>
       <main>
         {character ? (
-          <Container sx={{ py: 2 }} maxWidth="md">
+          <Container>
             <Grid container spacing={4}>
               <Grid item key={character.id} xs={12} sm={6} md={4}>
                 <Card
                   sx={{
                     height: "100%",
+                    width: 800,
                     display: "flex",
                     flexDirection: "column",
                   }}
                 >
                   <CardMedia
                     component="img"
-                    sx={{
-                      16: 9,
-                      //   pt: "56.25%",
-                    }}
+                    sx={{ width: 300, height: 400 }}
                     image={character.image}
-                    alt="random"
+                    alt="Live from space album cover"
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
@@ -80,6 +85,8 @@ export const Details = () => {
             </Grid>
           </Container>
         ) : (
+          // card prueba
+
           <h2>Cargando..</h2>
         )}
       </main>
